3867663f6dcb9bb6280ea21879e31d59b02c61d5
Greg Donnell <gregory.donnell74@gmail.com>
2018-06-26 20:38:49 -0600

    Create a very basic rough draft of the layout with mock values.  This includes some url routing, templating, and bringing bootstrap css library.

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'projects': [
+            {
+                'name': 'Project 1',
+                'githubLink': 'https://github.com/donnell74/myPortfolio',
+                'timelineData': '##data about timeline here##',
+                'icStatsData': '##data about ic stats here##',        
+                'reviewerStatsData': '##data about reviewer stats here##'
+            },
+            {
+                'name': 'Project 2',
+                'githubLink': 'https://github.com/donnell74/myPortfolio',
+                'timelineData': '##data about timeline here##',
+                'icStatsData': '##data about ic stats here##',        
+                'reviewerStatsData': '##data about reviewer stats here##'
+            },
+            {
+                'name': 'Project 3',
+                'githubLink': 'https://github.com/donnell74/myPortfolio',
+                'timelineData': '##data about timeline here##',
+                'icStatsData': '##data about ic stats here##',        
+                'reviewerStatsData': '##data about reviewer stats here##'
+            },
+        ]
+    }
+    return render(request, 'portfolio/index.html', context)

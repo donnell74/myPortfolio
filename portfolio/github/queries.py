fcbf688b428e5c6a01a074ca1341a4853b7b6958
Greg Donnell <gregory.donnell74@gmail.com>
2018-07-06 16:49:59 -0600

    Switch project query direction back to DESC.

--- portfolio/github/queries.py
+++ portfolio/github/queries.py
@@ -1,7 +1,7 @@
 LAST_25_PROJECTS_QUERY = """
 query { 
   viewer {
-    repositories (first: 25 orderBy: {field: CREATED_AT, direction: ASC} ) {
+    repositories (first: 25 orderBy: {field: CREATED_AT, direction: DESC} ) {
       nodes {
         id
         name
